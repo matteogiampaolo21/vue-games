1d21cd55e131f44975fb9af34d1440b53d5d8f1a
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2024-01-19 12:38:28 +0700

    added text signaling the winner and button to restart

--- src/views/ConnectFourView.vue
+++ src/views/ConnectFourView.vue
@@ -1,12 +1,13 @@
 <script>
 import {RouterLink} from 'vue-router'
-import {connectBoard, currentColor} from '../state/store'
+import {connectBoard, currentColor, connectResult} from '../state/store'
 
 export default {
     data(){
         return{
             connectBoard,
             currentColor,
+            connectResult
         }   
     },
     methods:{
@@ -20,7 +21,7 @@ export default {
                 this.checkColWin(col);
                 this.checkLeftDiagonalWin(row,col)
                 this.checkRightDiagonalWin(row,col)
-                this.currentColor == 'red' ? this.currentColor = 'blue' : this.currentColor = 'red';
+                this.currentColor == 'Red' ? this.currentColor = 'Blue' : this.currentColor = 'Red';
             }
         },
         checkRowWin(row,color){
@@ -30,7 +31,9 @@ export default {
                 if (circle.color == color){
                     count += 1;
                     if (count >= 4 ){
-                        alert(`Victory for ${color}`)
+                        this.connectResult.resultText = `${color}'s Victory! Try again?`
+                        this.connectResult.isDropDisabled = true;
+                        
                         return
                     }
                 }else{
@@ -49,7 +52,8 @@ export default {
             
             if (circle1 == circle2 && circle2 == circle3 && circle3 == circle4){
                 if(circle1 =='white'){return}
-                alert(`Victory for ${circle1}`)
+                this.connectResult.resultText = `${circle1}'s Victory! Try again?`
+                this.connectResult.isDropDisabled = true;
             }
            
             return
@@ -65,7 +69,8 @@ export default {
                     (this.connectBoard.connectRow2[col+2].color == this.connectBoard.connectRow3[col+1].color) && 
                     (this.connectBoard.connectRow3[col+1].color == this.connectBoard.connectRow4[col].color)
                 ){
-                    alert(`Victory for: ${this.connectBoard.connectRow4[col].color}`)
+                    this.connectResult.resultText = `${this.connectBoard.connectRow4[col].color}'s Victory! Try again?`
+                    this.connectResult.isDropDisabled = true;
                 }else{
                     console.log(col)
                     console.log(this.connectBoard.connectRow1[col].color)
@@ -86,7 +91,8 @@ export default {
                     (this.connectBoard.connectRow2[col-2].color == this.connectBoard.connectRow3[col-1].color) && 
                     (this.connectBoard.connectRow3[col-1].color == this.connectBoard.connectRow4[col].color)
                 ){
-                    alert(`Victory for: ${this.connectBoard.connectRow4[col].color}`)
+                    this.connectResult.resultText = `${this.connectBoard.connectRow4[col].color}'s Victory! Try again?`
+                    this.connectResult.isDropDisabled = true;
                 }else{
                     console.log(col)
                     console.log(this.connectBoard.connectRow1[col].color)
@@ -95,34 +101,50 @@ export default {
             }else{
                 console.log('not possible')
             }
+        },
+        clearBoard(){
+            console.log('e')
+            for (let i = 1; i < 5;i++){
+                for (const circle of this.connectBoard[`connectRow${i}`]){
+                    circle.color = 'white';
+                    circle.isFilled = false;
+                }
+            }
+            this.connectResult.isDropDisabled = false;
+            this.connectResult.resultText = '';
+            this.currentColor = 'Red';
         }
+        
     },
     components:{RouterLink}
 }
 </script>
 
 <template>
-  <header class="w-400 mx-auto">
+  <header class="w-400 mx-auto flex flex-col justify-center">
     <RouterLink to="/" class=" border-2 border-black p-1 fixed left-3 z-50 bg-white">Return to Home</RouterLink>
     <h1 class="text-4xl font-black text-center my-5">Connect Four</h1>
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nisi reprehenderit consectetur impedit veritatis quis iusto?</p>
+    <p id="end-text" class="text-center">
+        {{connectResult.resultText}}
+    </p>
+    <button @click="clearBoard" class="border-black border-2 px-3 py-1 ">Restart</button>
   </header>
 
   <main class="grid grid-cols-7 gap-6 justify-center items-center w-600 mt-20 mb-10 mx-auto">
-    <button @click="() => {onDropBtn(4,0)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,1)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,2)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,3)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,4)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,5)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
-    <button @click="() => {onDropBtn(4,6)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,0)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,1)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,2)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,3)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,4)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,5)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
+    <button :disabled="connectResult.isDropDisabled" @click="() => {onDropBtn(4,6)}" class="bg-neutral-200 border-2 h-5 rounded hover:bg-neutral-400 border-black"></button>
 
     
     
     </main>
 
     <section v-for="row in Object.values(connectBoard)" class="flex flex-row my-5  w-600 mx-auto">
-        <div v-for="circle in row" :class="`${circle.color} circle`">
+        <div v-for="circle in row" :class="`${circle.color } circle`">
         </div>
     </section> 
 </template>
@@ -139,13 +161,13 @@ export default {
     border-radius: 100%;
     margin: 0 auto;
 }
-.white{
+.White{
     background-color: #ffffff;
 }
-.red{
+.Red{
     background-color: #ca3b3b;
 }
-.blue{
+.Blue{
     background-color: #3f3fd2;
 }
 </style>
